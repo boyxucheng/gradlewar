19e6459877fd918458d1ab95d23a872ebba84f17
yhxu <419356700@qq.com>
2017-11-02 09:47:40 +0800

    x

---
+++ src/main/java/com/example/demo/GradleDemoWarTestApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleDemoWarTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleDemoWarTestApplication.class, args);
+	}
+}
